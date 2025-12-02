bcf77fd229abe1e1df104cf8c8f6e14fdbad876a
Oliver <oliveriida172@gmail.com>
2025-12-03 00:19:10 +0200

    backend works

--- backend/main.py
+++ backend/main.py
@@ -9,33 +9,51 @@ def check_car_number(car_number):
     if not car_number[3:].isalpha():
         return False
     return True
-    
+
+
+def current_cars():
+    print(f"Currently we have {len(cars)} cars parking. They are: {cars}")
+    print(f"Cars with their starting times: {cars_with_start_time}")
+
+
+def actions_input(owner_action, car_number):
+    if owner_action == "time":
+        print(f"The car {car_number} has been parked for {math.ceil(time.time() - cars_with_start_time[car_number])} seconds.\n")
+    elif owner_action == "end":
+        print(f"{car_number} parked for a total of {math.ceil(time.time() - cars_with_start_time[car_number])} seconds.\n")
+        del cars_with_start_time[car_number]
+        cars.remove(car_number)
+    else:
+        print("Wrong input!\n")
+
+
+def getting_owner_action(car_number):
+        if car_number in cars:
+            print("Type 'Time' to see how long the car has been parked for")
+            print("Type 'End' to stop parking")
+            owner_action = input("Select action: ")
+            owner_action = owner_action.lower()
+            return owner_action
+        elif car_number not in cars:
+            cars.append(car_number)
+            cars_with_start_time[car_number] = time.time()
+            print(f"Car {car_number} parked!\n")
+            return None
+
+
 cars = []
+cars_with_start_time = {}
+
 
 while True:
     car_number = input("Enter your car number: ")
-    if check_car_number(car_number) == True:
-        print("Car number successfully entered!\n")
-        cars.append(car_number)
-        break
-    elif check_car_number(car_number) == False:
-        print("NB! The car number must be in the following format: 123ABC\n")
-
-parking_started = time.time()
-
-while True:
-    owner_action = input("Type 'TIME' to see how long the car has been parked or 'END' to stop parking: ")
-
-    if owner_action == "TIME":
-        print(f"The car {car_number} has been parked for {math.ceil(time.time() - parking_started)} seconds.\n")
-    elif owner_action == "END":
-        print(f"{car_number} parked for a total of {math.ceil(time.time() - parking_started)} seconds.\n")
-        break
+    if check_car_number(car_number):
+        owner_action = getting_owner_action(car_number)
+        if owner_action == "time" or owner_action == "end":
+            actions_input(owner_action, car_number)
+        elif owner_action == None:
+            continue
     else:
-        print("Wrong input!\n")
+        print("NB! The car number must be in the following format: 123ABC\n")
         continue
 
-
-print(cars)
-
-#print("Until next time:)")
